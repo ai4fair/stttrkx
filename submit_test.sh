0fe65b651cf03e0781a76aa0a208dc8d5d04e2ed
Adeel Akram <adeel.chep@gmail.com>
2022-05-10 13:13:14 +0200

    submit_test.sh is completely modified.

--- submit_test.sh
+++ submit_test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Export Variables
-# export LUSTRE_HOME=/lustre/$(id -g -n)/$USER
-# export EXATRKX_DATA=$LUSTRE_HOME/ctd2022
+export LUSTRE_HOME=/lustre/$(id -g -n)/$USER
+export SLURM_SUBMIT_DIR=$LUSTRE_HOME"/ctd2022"
+export EXATRKX_DATA=$SLURM_SUBMIT_DIR
+export SLURM_WORKING_DIR=$LUSTRE_HOME"/ctd2022/logs"
+mkdir -p $SLURM_WORKING_DIR;
 
-#SBATCH -A panda -J CTD22 -p main -c32 -n1 -N1 --gres=gpu:tesla:1 --mem=32768 -t 0:30:00
-srun singularity run --nv $LUSTRE_HOME/containers/gpu_stttrkx.sif -c "conda activate exatrkx && python test.py"
+CENV= exatrkx
+CONT=gpu_stttrkx.sif
+
+srun --get-user-env -t 00:30 -o '%x-%j.out' -e '%x-%j.err' -J Test -D $SLURM_WORKING_DIR -- singularity run --nv $LUSTRE_HOME/containers/$CONT -c "conda activate $CENV && python test.py"
