652c4ebbb591edac0c7e3afb125a3cabe6a6c9f8
Adeel Akram <adeel.chep@gmail.com>
2022-05-10 15:27:07 +0200

    Test Script is Simplified and Modified

--- submit_test.sh
+++ submit_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH -A panda -J Test -t 00:30 -p main -N1 -c16 --gres=gpu:tesla:1 -D . -o '%x-%j.out' -e '%x-%j.err'
+#SBATCH -A panda -J Test -t 12:30 -p main -N1 -c16 --gres=gpu:tesla:1 --mail-type=END --mail-user=a.akram@gsi.de
 
 export LUSTRE_HOME=/lustre/$(id -g -n)/$USER
 export EXATRKX_DATA=$LUSTRE_HOME/ctd2022
 
 CENV=exatrkx
-CONT=gpu_stttrkx.sif
-#singularity run --nv $LUSTRE_HOME/containers/$CONT -c "conda activate $CENV && traintrack configs/pipeline_quickstart.yaml"
-singularity run --nv /lustre/pbar/aakram/containers/$CONT -c "conda activate $CENV && python test.py"
+CONT=$LUSTRE_HOME/containers/gpu_stttrkx.sif
 
+#singularity run --nv $CONT -c "conda activate $CENV && python test.py"
+singularity run --nv $CONT -c "conda activate $CENV && traintrack configs/pipeline_quickstart.yaml"
