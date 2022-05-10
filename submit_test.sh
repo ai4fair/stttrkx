0022c0744388c0a1aff37ae743c431656a6ba7e3
Adeel Akram <adeel.chep@gmail.com>
2022-05-10 13:16:42 +0200

    submit_test.sh is fixed

--- submit_test.sh
+++ submit_test.sh
@@ -10,4 +10,4 @@ mkdir -p $SLURM_WORKING_DIR;
 CENV= exatrkx
 CONT=gpu_stttrkx.sif
 
-srun --get-user-env -t 00:30 -o '%x-%j.out' -e '%x-%j.err' -J Test -D $SLURM_WORKING_DIR -- singularity run --nv $LUSTRE_HOME/containers/$CONT -c "conda activate $CENV && python test.py"
+srun -A panda -J Test -t 00:30 -o '%x-%j.out' -e '%x-%j.err' -D $SLURM_WORKING_DIR -- singularity run --nv $LUSTRE_HOME/containers/$CONT -c "conda activate $CENV && python test.py"
