35ea282c08ec01d5af48d89a54138460140c91b9
Adeel Akram <adeel.chep@gmail.com>
2022-05-19 16:04:41 +0200

    Script for plotting track efficiency and purity

---
+++ eval/plot_trk_perf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script runs 'plot_trk_perf.py'
+
+infile="run/trkx_reco_eval/eval_particles.h5"
+outputdir="run/trkx_reco_eval"
+prefix="trk_perf"
+
+# reco tracks from GNN
+python plot_trk_perf.py -f $infile -o $outputdir -p $prefix
