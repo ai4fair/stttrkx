7128d2f835eb6613de52ac883a4c50bfcd6523d5
Adeel Akram <adeel.chep@gmail.com>
2023-01-04 12:34:50 +0100

    Layer norm in Edge Classifier

--- LightningModules/DNN/Models/dense_network.py
+++ LightningModules/DNN/Models/dense_network.py
@@ -12,26 +12,30 @@ from ..utils.dnn_utils import make_mlp
 
 # Edge Classification
 class EdgeClassifier(DNNBase):
-    """A Deep Neural Network (a.k.a DFF, MLP, etc) class"""
+    """A Deep Neural Network for Edge Classification"""
     
     def __init__(self, hparams):
         super().__init__(hparams)
-        """Initialise the Network that can Classifiy Edges"""
         
         # Input Size: 2*(Node Features)
         input_dim = (hparams["spatial_channels"] + hparams["cell_channels"])*2
         
         # Create a Dense Network for Edge Classification
-        self.network = make_mlp(input_size=input_dim,
-                                sizes=[1000,2000,2000,2000,1000,1],
-                                hidden_activation="ReLU",
-                                output_activation=None,
-                                layer_norm=False
+        self.network = make_mlp(input_size=input_dim,                           # features  
+                                sizes=[1000,2000,2000,2000,1000,1],             # Nodes
+                                hidden_activation=hparams["hidden_activation"], # Relu
+                                output_activation=None,                         # None
+                                layer_norm=hparams["layernorm"]                 # Layer Norm
                                 )
     
     
     def forward(self, x, edge_index):
+    
+        # indices (node_1 + node_2)
         start, end = edge_index
+        
+        # edges (node_1 + node_2) 
         edge_inputs = torch.cat([x[start], x[end]], dim=1)
+        
         return self.network(edge_inputs)
 
