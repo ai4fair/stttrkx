1fcab8b9856f129a08d30fe569009463d3e42ecf
Adeel Akram <adeel.chep@gmail.com>
2023-01-30 17:43:51 +0100

    Working DNN, GNN Stages with Old Training Checkpoints

--- LightningModules/DNN/Models/dense_network.py
+++ LightningModules/DNN/Models/dense_network.py
@@ -21,11 +21,11 @@ class EdgeClassifier(DNNBase):
         input_dim = (hparams["spatial_channels"] + hparams["cell_channels"])*2
         
         # Create a Dense Network for Edge Classification
-        self.network = make_mlp(input_size=input_dim,                           # features  
-                                sizes=[1000,2000,2000,2000,1000,1],             # Nodes
-                                hidden_activation=hparams["hidden_activation"], # Relu
-                                output_activation=None,                         # None
-                                layer_norm=hparams["layernorm"]                 # Layer Norm
+        self.network = make_mlp(input_size=input_dim,
+                                sizes=[1000,2000,2000,2000,1000,1],
+                                hidden_activation="ReLU",
+                                output_activation=None,
+                                layer_norm=False
                                 )
     
     
