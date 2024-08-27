eb9cd81c5b28594d91c843764758c300af16664d
Nikolai in der Wiesche <n_inde01@uni-muenster.de>
2024-08-27 14:52:25 +0200

    Merged the newProcessing dir of "nikolaisProject" into the "upgrading" branch. Also renamed the LightningModule directories, and added my new config files.

--- LightningModules/Processing/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Fucntions accessible from Processing/
-from .utils.data_utils import SttTorchDataReader
-from .utils.draw_utils import cylindrical_to_cartesian
-from .utils.draw_utils import draw_proc_event
-from .utils.event_utils import graph_intersection
