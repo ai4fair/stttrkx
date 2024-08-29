a6ac1e5b85b1ac41d8091eeb6d1ac9ca2a2c19ad
Adeel Akram <adeel.chep@gmail.com>
2024-08-29 18:05:59 +0200

    graph construction: added same layer edges in the layerwise heuristic

--- src/drawing.py
+++ src/drawing.py
@@ -19,7 +19,7 @@ except KeyError as e:
 detector_path = os.path.join(output_base, 'stt.csv')
 
 
-def Visualize_Edges (event, edges, figsize=(10,10), fig_type="pdf", save_fig=False):
+def Visualize_Edges (event, edges, figsize=(10,10), fig_type="pdf", save_fig=False, annotate=False):
     """Function to plot nodes and edges of an event."""
 
     # detector layout
@@ -32,16 +32,26 @@ def Visualize_Edges (event, edges, figsize=(10,10), fig_type="pdf", save_fig=Fal
     # draw particles (event)
     unique_pids = np.unique(event.particle_id).astype(int)
     for pid in unique_pids:
-        mask = (event.particle_id == pid)
-        ax.scatter(event[mask].x.values, event[mask].y.values, color=cmap[pid], label=f'particle_id: {pid}')
+        mask = (event.particle_id == pid)  # filter each pid
+        color = cmap[pid % len(cmap)]  # cycle through cmap if pid exceeds length
+        ax.scatter(event[mask].x.values, event[mask].y.values, color=color, label=f'particle_id: {pid}')
 
     # draw edges (edges) 
     # for i, (source_node, target_node) in enumerate(true_edges.T):
     for (source_node, target_node) in edges.T:
+        
+        # get source and target positions
         source_pos = event.loc[source_node, ['x', 'y']].values
         target_pos = event.loc[target_node, ['x', 'y']].values
-        ax.plot([source_pos[0], target_pos[0]], [source_pos[1], target_pos[1]], 'k-', linewidth=0.5)
-
+        ax.plot([source_pos[0], target_pos[0]], [source_pos[1], target_pos[1]], 'k-', lw=0.5)
+        
+        if annotate:
+            # annotate with an arrow
+            ax.annotate("",
+                xy=(target_pos[0], target_pos[1]), xycoords='data',
+                xytext=(source_pos[0], source_pos[1]), textcoords='data',
+                arrowprops=dict(arrowstyle="->", lw=0.5))
+            
     # axis params
     evtid = event.event_id.unique()[0]
     ax.set_title('Event # {}'.format(evtid))
@@ -50,7 +60,7 @@ def Visualize_Edges (event, edges, figsize=(10,10), fig_type="pdf", save_fig=Fal
 
     # save figure
     if save_fig:
-        fig.savefig('event_{}.{}'.format(evtid, fig_type))
+        fig.savefig(f"event_{evtid}.{fig_type}", format=fig_type)
 
 
 # Draw SttCSVDataReader Event:: Using Object-Oriented API
