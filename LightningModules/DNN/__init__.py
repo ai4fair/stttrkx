e9cf6feaf8e3e22fbc715ccf358057e289bf19dd
Adeel Akram <adeel.chep@gmail.com>
2024-08-29 10:54:06 +0200

    DNN module is merged with Edge Labelling.

--- LightningModules/DNN/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from .Models.dense_network import EdgeClassifier, EdgeClassifier_BN, EdgeClassifier_LN
-from .Models.inference import GNNTelemetry, GNNBuilder
-from .Models.infer import GNNMetrics
--- LightningModules/edge_labelling/__init__.py
+++ LightningModules/edge_labelling/__init__.py
@@ -4,12 +4,15 @@
 # interaction gnn
 from .Models.interaction_gnn import InteractionGNN
 
-# residual attention gnn
+# residual attention gnn (modified version of GAT under MPNN)
 from .Models.residual_checkagnn import CheckResAGNN
 
-# residual attention gnn
+# residual convolutional gnn
 from .Models.residual_checkgcn import CheckResGCN
 
 # inference callbacks
 from .Models.inference import GNNTelemetry, GNNBuilder
 from .Models.infer import GNNMetrics
+
+# dense network
+from .Models.dense_network import EdgeClassifier, EdgeClassifier_BN, EdgeClassifier_LN
