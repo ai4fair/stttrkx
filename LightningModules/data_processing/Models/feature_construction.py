2be17c8218b90bc5d045e41d135fd952c4e58401
Adeel Akram <adeel.chep@gmail.com>
2024-09-13 11:20:57 +0200

    feature_construction now use trackml_event_utils instead of event_utils

--- LightningModules/data_processing/Models/feature_construction.py
+++ LightningModules/data_processing/Models/feature_construction.py
@@ -7,21 +7,22 @@ from functools import partial
 from tqdm.contrib.concurrent import process_map
 
 from ..feature_store_base import FeatureStoreBase
-from ..utils.event_utils  import prepare_event as trackml_prepare_event
+from ..utils.trackml_event_utils import prepare_event as trackml_prepare_event
 from ..utils.panda_event_utils import prepare_event as panda_prepare_event
 from ..utils.root_file_reader import ROOTFileReader
+
 
 class TrackMLFeatureStore(FeatureStoreBase):
     """
     Processing model to convert STT data into files ready for GNN training
-    
+
     Description:
         This class is used to read data from csv files containing PANDA STT hit and MC truth information.
         This information is then used to create true and input graphs and saved in PyTorch geometry files.
         It is a subclass of FeatureStoreBase which in turn is a subclass of the PyTorch Lighting's LightningDataModule.
     """
 
-    def __init__(self, hparams : dict):
+    def __init__(self, hparams: dict):
         """
         Initializes the TrackMLFeatureStore class.
 
@@ -37,9 +38,9 @@ class TrackMLFeatureStore(FeatureStoreBase):
     def prepare_data(self):
         """
         Main function for the feature construction / data processing.
-        
+
         Description:
-            Parallelizes the processing of input files by splitting them into 
+            Parallelizes the processing of input files by splitting them into
             evenly sized chunks and processing each chunk in parallel.
         """
 
@@ -53,7 +54,9 @@ class TrackMLFeatureStore(FeatureStoreBase):
 
         # Find the input files
         all_files = os.listdir(self.input_dir)
-        all_events = sorted(np.unique([os.path.join(self.input_dir, event[:15]) for event in all_files]))[: self.n_files]
+        all_events = sorted(
+            np.unique([os.path.join(self.input_dir, event[:15]) for event in all_files])
+        )[: self.n_files]
 
         # Split the input files by number of tasks and select my chunk only
         all_events = np.array_split(all_events, self.n_tasks)[self.task]
@@ -61,11 +64,18 @@ class TrackMLFeatureStore(FeatureStoreBase):
         # Process input files with a worker pool and progress bar
         # Use process_map() from tqdm instead of mp.Pool from multiprocessing.
         process_func = partial(trackml_prepare_event, **self.hparams)
-        process_map(process_func, all_events, max_workers=self.n_workers, chunksize=self.chunksize)
+        process_map(
+            process_func,
+            all_events,
+            max_workers=self.n_workers,
+            chunksize=self.chunksize,
+        )
 
         # Print the time taken for feature construction
         end_time = time()
-        print(f"Feature construction complete. Time taken: {end_time - start_time:f} seconds.")
+        print(
+            f"Feature construction complete. Time taken: {end_time - start_time:f} seconds."
+        )
 
 
 class PandaFeatureStore(FeatureStoreBase):
